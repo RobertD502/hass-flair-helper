0b065f450254a3453d6c06249266a0690e29860c
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-04-20 20:15:00 -0400

    Add files via upload

---
+++ flair/vents/vent.py
@@ -0,0 +1,48 @@
+import time
+import collections
+
+
+class Vent(object):
+    def __init__(self, data, api):
+        self.api = api
+        self.vent_id = data.id_
+        self.vent_name = data.attributes['name']
+        self.refresh()
+
+    def refresh(self):
+        vent_state = self.api.refresh_attributes('vents', self.vent_id)
+        self.voltage = vent_state.attributes['voltage']
+        self.is_vent_open = vent_state.attributes['percent-open'] == 100 or vent_state.attributes['percent-open'] == 50
+        self.vent_percent = vent_state.attributes['percent-open']
+        self.is_active = vent_state.attributes['inactive'] == False
+        self.rssi = vent_state.attributes['current-rssi']
+
+    def set_vent_open(self):
+        resource_type = 'vents'
+        attributes = {
+        'percent-open': 100,
+        }
+        relationships = {}
+        self.api.control_vent(self, resource_type, attributes, relationships)
+        self.refresh()
+
+    def set_vent_closed(self):
+        resource_type = 'vents'
+        attributes = {
+        'percent-open': 0,
+        }
+        relationships = {}
+        self.api.control_vent(self, resource_type, attributes, relationships)
+        self.refresh()
+
+    def set_vent_half(self):
+        resource_type = 'vents'
+        attributes = {
+        'percent-open': 50,
+        }
+        relationships = {}
+        self.api.control_vent(self, resource_type, attributes, relationships)
+        self.refresh()
+
+#Office-Right
+#{'setup-lightstrip': 1, 'created-at': '2019-12-03T02:02:40.847808+00:00', 'voltage': 3.06, 'inactive': False, 'percent-open-reason': 'This room does not require heating. Vent will open when the room is below 67.0F.', 'updated-at': '2021-04-20T04:09:55.122649+00:00', 'percent-open': 0, 'name': 'Office-Right', 'has-buzzed': False, 'current-rssi': -87.0}
