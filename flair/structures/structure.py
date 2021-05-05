94e1babad1efe4258a1c44d17507c92a8dc5dd8d
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-05-05 18:37:39 -0400

    Update structure.py

--- flair/structures/structure.py
+++ flair/structures/structure.py
@@ -1,7 +1,7 @@
 import time
 import collections
 
-#Possible use in future
+
 class Structure(object):
     def __init__(self, data, api):
         self.api = api
@@ -12,3 +12,32 @@ class Structure(object):
     def refresh(self):
         structure_state = self.api.refresh_attributes('structures', self.structure_id)
         self.current_hvac_mode = structure_state.attributes['structure-heat-cool-mode']
+        self.relationships = structure_state.relationships
+        self.active_schedule = structure_state.attributes['active-schedule-id']
+        try:
+            structure_schedules = self.api.get_schedules(self.structure_id)
+            """Get all schedules as dictionaries and place in a list"""
+            sl = []
+            for schedule in structure_schedules.resources:
+                sl.append({
+                    schedule.attributes['name']: schedule.id_,
+                })
+            self.schedules_list = sl
+            """Combine all the schedule dictionaries into a single dictionary"""
+            cs = {}
+            for dictc in sl:
+                cs.update(dictc)
+            self.combined_schedules = cs
+        except AttributeError:
+            print("This structure doesn't have any attributes")
+        except:
+            print("Something went wrong")
+    
+    def set_schedule(self, schedule_id):
+        resource_type = 'structures'
+        attributes = {
+        'active-schedule-id': schedule_id,
+        }
+        relationships = {}
+        self.api.control_schedule(self, resource_type, attributes, relationships)
+        self.refresh()
