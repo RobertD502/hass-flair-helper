fd6e4f63b06d5f2eb058a3e541d5b1319eb1e168
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-11-25 01:37:32 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hass-flair-helper",
-    version="0.0.8",
+    version="0.0.9",
     author="Robert Drinovac",
     author_email="unlisted@gmail.com",
     description="A Python library for home-assistant-flair to help utilize the Flair Python API ",
