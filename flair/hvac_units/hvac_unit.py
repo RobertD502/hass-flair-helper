38b9b784aa36646a88176e051cfa84028df89d14
shaun <smcpeck@gmail.com>
2022-01-12 20:16:25 -0600

    No need for next/iter/values since supported temps is already a list

--- flair/hvac_units/hvac_unit.py
+++ flair/hvac_units/hvac_unit.py
@@ -63,12 +63,12 @@ class HvacUnit(object):
         ### Create list of supported HVAC temps in different modes ###
         fan_key = f"FAN {self.hvac_fan_speed.upper()}"
         if self.hvac_capabilities == "Both":
-            self.supported_heat_temps = next(iter(hvac_state.attributes['constraints']['ON']['HEAT']['ON'][fan_key].values()))
-            self.supported_cool_temps = next(iter(hvac_state.attributes['constraints']['ON']['COOL']['OFF'][fan_key].values()))
+            self.supported_heat_temps = hvac_state.attributes['constraints']['ON']['HEAT']['ON'][fan_key]
+            self.supported_cool_temps = hvac_state.attributes['constraints']['ON']['COOL']['OFF'][fan_key]
         elif self.hvac_capabilities == "Heat":
-            self.supported_heat_temps = next(iter(hvac_state.attributes['constraints']['ON']['HEAT']['ON'][fan_key].values()))
+            self.supported_heat_temps = hvac_state.attributes['constraints']['ON']['HEAT']['ON'][fan_key]
         else:
-            self.supported_cool_temps = next(iter(hvac_state.attributes['constraints']['ON']['COOL']['OFF'][fan_key].values()))
+            self.supported_cool_temps = hvac_state.attributes['constraints']['ON']['COOL']['OFF'][fan_key]
 
     ### Power can be either On or Off ###
     def set_hvac_power(self, power):
